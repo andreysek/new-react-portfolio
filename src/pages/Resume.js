608557a5720acbc0130225ab6c7067d53e0e58ed
Sekitto Andrew <andreysek@icloud.com>
2022-09-22 12:06:53 +0300

    More Commits
    Added more skills and certification

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { SiGithub, SiLinkedin, SiDiscord, SiHackerrank, SiReact, SiJavascript, SiPython, SiCss3, SiHtml5, SiSwift, SiNodemon, SiBootstrap, SiTwitter, SiIos, SiSteam } from 'react-icons/si';
+import { FaAws } from "react-icons/fa"
 import { TypeAnimation } from 'react-type-animation';
 
 
@@ -20,7 +21,7 @@ function Resume() {
                                     sequence={[
                                         'SEKITTO ANDREW',
                                         2000,
-                                      
+
                                     ]}
                                     wrapper="h2"
                                     repeat={Infinity}
@@ -34,17 +35,17 @@ function Resume() {
                     <div className="row">
                         <div className="col-md-6">
                             <h4 className="ff-jose ls-2"> <TypeAnimation
-                                    cursor={false}
-                                    sequence={[
-                                        'EXPERIENCE',
-                                        2000,
-                                      
-                                    ]}
-                                    wrapper="h2"
-                                    repeat={Infinity}
-                                    style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+                                cursor={false}
+                                sequence={[
+                                    'EXPERIENCE',
+                                    2000,
 
-                                /></h4>
+                                ]}
+                                wrapper="h2"
+                                repeat={Infinity}
+                                style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+
+                            /></h4>
                             <ul>
                                 <li>
                                     <h4 className="ff-jose my-1 text-black">Information Technology Specialist</h4>
@@ -58,17 +59,17 @@ function Resume() {
                         <div className="col-md-6">
 
                             <h4 className="ff-jose ls-2"> <TypeAnimation
-                                    cursor={false}
-                                    sequence={[
-                                        'EDUCATION',
-                                        2000,
-                                      
-                                    ]}
-                                    wrapper="h2"
-                                    repeat={Infinity}
-                                    style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+                                cursor={false}
+                                sequence={[
+                                    'EDUCATION',
+                                    2000,
 
-                                /></h4>
+                                ]}
+                                wrapper="h2"
+                                repeat={Infinity}
+                                style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+
+                            /></h4>
                             <ul>
                                 <li>
                                     <h4 className="ff-jose my-1 text-black">Bachelor in Business Computing</h4>
@@ -83,17 +84,17 @@ function Resume() {
                         <div className="col-md-6">
 
                             <h4 className="ff-jose ls-2"> <TypeAnimation
-                                    cursor={false}
-                                    sequence={[
-                                        'SKILLS',
-                                        2000,
-                                      
-                                    ]}
-                                    wrapper="h2"
-                                    repeat={Infinity}
-                                    style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+                                cursor={false}
+                                sequence={[
+                                    'SKILLS',
+                                    2000,
 
-                                /></h4>
+                                ]}
+                                wrapper="h2"
+                                repeat={Infinity}
+                                style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+
+                            /></h4>
                             <ul>
                                 <li>
                                     <h4 className="white-label px-2 py-1"><SiReact /> React JS </h4>
@@ -105,10 +106,10 @@ function Resume() {
                                     <h4 className="white-label px-2 py-1"><SiCss3 /> CSS 3 </h4>
                                 </li>
                                 <li>
-                                    <h4 className="white-label px-2 py-1"><SiHtml5 /> HTML 5 </h4>
+                                    <h4 className="white-label px-2 py-1"><SiHtml5 /> HTML 5/Web Scrapping </h4>
                                 </li>
                                 <li>
-                                    <h4 className="white-label px-2 py-1"><SiSwift /> SWIFT </h4>
+                                    <h4 className="white-label px-2 py-1"><SiSwift /> SWIFT/AppleGenius </h4>
                                 </li>
                                 <li>
                                     <h4 className="white-label px-2 py-1"><SiNodemon /> NODE JS </h4>
@@ -117,16 +118,16 @@ function Resume() {
                                     <h4 className="white-label px-2 py-1"><SiBootstrap /> BOOTSTRAP </h4>
                                 </li>
                                 <li>
-                                    <h4 className="white-label px-2 py-1"><SiPython />PYTHON</h4>
+                                    <h4 className="white-label px-2 py-1"><SiPython />PYTHON/FLASK</h4>
                                 </li>
                                 <li>
-                                    <h4 className="white-label px-2 py-1"><SiGithub /> GITHUB </h4>
+                                    <h4 className="white-label px-2 py-1"><SiGithub /> GIT </h4>
                                 </li>
                                 <li>
                                     <h4 className="white-label px-2 py-1"><SiIos /> APPLE GENIUS </h4>
                                 </li>
                                 <li>
-                                    <h4 className="white-label px-2 py-1"><SiSteam /> TEAM WORKER </h4>
+                                    <h4 className="white-label px-2 py-1"><FaAws /> AWS Cloud </h4>
                                 </li>
 
                             </ul>
@@ -136,18 +137,24 @@ function Resume() {
                         <div className="col-md-6">
 
                             <h4 className="ff-jose ls-2"> <TypeAnimation
-                                    cursor={false}
-                                    sequence={[
-                                        'LICENSES & CERTIFICATIONS',
-                                        2000,
-                                      
-                                    ]}
-                                    wrapper="h2"
-                                    repeat={Infinity}
-                                    style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+                                cursor={false}
+                                sequence={[
+                                    'LICENSES & CERTIFICATIONS',
+                                    2000,
 
-                                /></h4>
+                                ]}
+                                wrapper="h2"
+                                repeat={Infinity}
+                                style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+
+                            /></h4>
                             <ul>
+                                <li>
+                                    <h4 className="ff-jose my-1 text-black">
+                                    AWS Training and Certification</h4>
+                                    <h6 className="white-label px-2 py-1"><a href='https://explore.skillbuilder.aws/learn/course/134/aws-cloud-practitioner-essentials'>AWS Cloud Practitioner Essentials</a></h6>
+                                </li>
+
                                 <li>
                                     <h4 className="ff-jose my-1 text-black">The Complete 2022 Web Development BootCamp </h4>
                                     <h6 className="white-label px-2 py-1">Certificate ID</h6>
@@ -187,17 +194,17 @@ function Resume() {
                     <div className="row">
                         <div className="col-md-6">
                             <h4 className="ff-jose ls-2"> <TypeAnimation
-                                    cursor={false}
-                                    sequence={[
-                                        'PROFILE',
-                                        2000,
-                                      
-                                    ]}
-                                    wrapper="h2"
-                                    repeat={Infinity}
-                                    style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+                                cursor={false}
+                                sequence={[
+                                    'PROFILE',
+                                    2000,
 
-                                /></h4>
+                                ]}
+                                wrapper="h2"
+                                repeat={Infinity}
+                                style={{ fontSize: '1.5em', color: 'rgb(34, 32, 32)', letterSpacing: '-2px', fontFamily: 'Rubik Maze', fontWeight: 'lighter' }}
+
+                            /></h4>
                             <h1>
                                 <a href="https://github.com/andreysek" target="/blank" className="text-black text-decoration-none mx-2">
                                     <SiGithub className="zoom-on-hover" />
